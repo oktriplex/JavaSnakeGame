ff2c38098bc1786a72122719d218336cc79fc859
oktriplex <80281764+oktriplex@users.noreply.github.com>
2021-08-10 09:42:34 +0300

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class GameFrame extends JFrame {
     GameFrame() {
         this.add(new GamePanel());
-        this.setTitle("Змейка");
+        this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
